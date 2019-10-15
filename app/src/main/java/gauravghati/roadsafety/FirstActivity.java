652a120793e1c0fa7e0dc8a3dd74e0e9de6ae84b
gauravghati <gauravghati225@gmail.com>
2019-10-15 22:48:11 +0530

    Road Safety Complete app

---
+++ app/src/main/java/gauravghati/roadsafety/FirstActivity.java
@@ -0,0 +1,53 @@
+package gauravghati.roadsafety;
+
+import android.content.Intent;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.net.Uri;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class FirstActivity extends AppCompatActivity {
+    Button button,button2;
+    private LocationManager locationManager;
+    private LocationListener locationListner;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.firstactivity);
+        button = (Button) findViewById(R.id.button);
+        button2 = (Button) findViewById(R.id.button2);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = null;
+                intent= new Intent (Intent.ACTION_VIEW);
+                intent.setData(Uri.parse("geo:18.5204,73.8567"));
+                startActivity(intent);
+            }
+
+        });
+        button2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+             // In Toast.makeText(getApplicationContext(),"USER ID AND PASS IS CORRECT", Toast.LENGTH_LONG).show();
+                Intent intent=new Intent(FirstActivity.this,MainActivity.class);
+                startActivity(intent);
+
+
+            }
+            });
+        }
+
+
+
+    }
+
+
+
+
+
+
---
+++ app/src/main/java/gauravghati/roadsafety/activ_2.java
@@ -0,0 +1,15 @@
+package gauravghati.roadsafety;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class activ_2 extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activ2);
+    }
+
+}
+
+
